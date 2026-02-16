8cf41681c706faae0b53347f7302bc7ef25634b5
Giada De Martino <giada-de-martino@etu.umontpellier.fr>
2026-02-16 16:43:21 +0100

    init

---
+++ app/script.js
@@ -0,0 +1,5 @@
+// Simple form submit alert (can enhance later)
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you! Your message has been sent.');
+});
